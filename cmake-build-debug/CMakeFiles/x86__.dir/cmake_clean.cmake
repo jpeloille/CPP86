737399c9762f4914340389f4cf1f4033a20ba674
julien <jpeloille@lagoon.nc>
2021-05-21 09:23:52 +1100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/x86__.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/x86__.dir/main.cpp.o"
+  "CMakeFiles/x86__.dir/src/iapx86.cpp.o"
+  "x86__"
+  "x86__.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/x86__.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
